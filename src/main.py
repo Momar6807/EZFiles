494ec2e8eb8c24f345fee628cce7567ca57044cf
Momar6807 <112126020+Momar6807@users.noreply.github.com>
2025-06-21 01:22:40 -0600

    UPDATE LICENSE

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
-# culo el q se lo robe perro @Momar64 2025
+"""Copyright (C) 2025  Omar Martinez @Momar6807
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    """
 
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
