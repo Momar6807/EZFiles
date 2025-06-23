3d01b8149f3ef6494aa260cb5a72f6df824020eb
Momar6807 <112126020+Momar6807@users.noreply.github.com>
2025-06-22 20:37:21 -0600

    minor update
    Se cambia el export.py para la nueva configuración del archivo .spec

--- src/export.py
+++ src/export.py
@@ -6,12 +6,13 @@ print(dir)
 
 # ejecutar pyinstaller
 
-response = subprocess.run(
-    ["pyinstaller", "--onefile", "--windowed", "--name", "EZFiles",
-        "--icon", "icono.ico", "--add-data", "icono.ico;.", "main.py"],
-    stdout=subprocess.PIPE,
-    cwd=dir
-)
+# response = subprocess.run(
+#     ["pyinstaller", "--onefile", "--windowed", "--name", "EZFiles",
+#         "--icon", "icono.ico", "--add-data", "icono.ico;.", "main.py"],
+#     stdout=subprocess.PIPE,
+#     cwd=dir
+# )
 
+response = subprocess.run(["pyinstaller", "EZFiles.spec"])
 
-print(response.stdout.decode('utf-8'))
+# print(response.stdout.decode('utf-8'))
